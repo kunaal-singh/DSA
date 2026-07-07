e5affe9c30a1855e54758bf5aaaf7830f4a6fd81
kunaal-singh <kunaalsingh1637@gmail.com>
2026-07-07 23:57:37 +0530

    test

---
+++ ARRAYS/26_Check_if_array_sorted_roated/question.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Function to check if the array is sorted and rotated
+bool check(vector<int>& nums)
+{
+    int count = 0;
+    int n = nums.size();
+
+    // Count the number of decreasing points
+    for (int i = 1; i < n; i++)
+    {
+        if (nums[i - 1] > nums[i])
+        {
+            count++;
+        }
+    }
+
+    // Check the last and first element
+    if (nums[n - 1] > nums[0])
+    {
+        count++;
+    }
+
+    // If there is at most one decreasing point, return true
+    return count <= 1;
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    bool ans = check(nums);
+
+    if (ans)
+    {
+        cout << "True" << endl;
+    }
+    else
+    {
+        cout << "False" << endl;
+    }
+
+    return 0;
+}
