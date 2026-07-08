519e81985a1fb5e93051a40bbad8af4930f939b5
kunaal-singh <kunaalsingh1637@gmail.com>
2026-07-08 23:25:37 +0530

    Code

---
+++ ARRAYS/27_Sort_Colours/question.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void sortColors(vector<int>& arr)
+{
+    int low = 0;
+    int mid = 0;
+    int high = arr.size() - 1;
+
+    while (mid <= high)
+    {
+        if (arr[mid] == 0)
+        {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1)
+        {
+            mid++;
+        }
+        else // arr[mid] == 2
+        {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter " << n << " elements (0, 1, or 2): ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sortColors(arr);
+
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
