f9259a46c499d38a594aa4ed21b08d01dd1d8568
kunaal-singh <kunaalsingh1637@gmail.com>
2026-07-09 23:23:50 +0530

    test

---
+++ ARRAYS/28_Chocolate_Problem/question.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findMinDiff(vector<int>& arr, int m) {
+        int n = arr.size();
+
+        if (m == 0 || n == 0)
+            return 0;
+
+        if (m > n)
+            return -1;
+
+        sort(arr.begin(), arr.end());
+
+        int ans = INT_MAX;
+
+        for (int i = 0; i <= n - m; i++) {
+            ans = min(ans, arr[i + m - 1] - arr[i]);
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> arr = {7, 3, 2, 4, 9, 12, 56};
+    int m = 3;
+
+    cout << obj.findMinDiff(arr, m);
+
+    return 0;
+}
